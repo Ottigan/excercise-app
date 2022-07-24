e0917b44356d14f1185d0a6142562e3e9398cc76
Jānis Malcāns <janis.malcans@dtg.lv>
2022-07-25 01:53:29 +0300

    UPDATE GithubProvider to return emails for User uniqueness

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,5 +7,10 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button = (props: ButtonProps): React.ReactElement => {
   const { children } = props;
 
-  return <button className="h-8 border-2 rounded-md px-3 bg-white" {...props}>{children}</button>;
+  return (
+    <button
+      className="h-8 border-2 rounded-md px-3 bg-white text-black font-medium" {...props}>
+      {children}
+    </button>
+  );
 };
